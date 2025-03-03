16445ea91ace81e62321003040240e15c2d31aff
santeri3700 <santeri.pikarinen@gmail.com>
2025-03-03 20:32:27 +0200

    Add FFMPEG_BIN and FFMPEG_ARGS environment variables for FFMPEG customization

--- src/config.rs
+++ src/config.rs
@@ -5,6 +5,8 @@ pub struct Config {
     pub discord_token: String,
     pub download_dir: String,
     pub converted_dir: String,
+    pub ffmpeg_bin: String,
+    pub ffmpeg_args: String,
     pub host: String,
     pub port: u16,
     pub public_url: String,
@@ -17,6 +19,24 @@ impl Config {
         let data_path = env::var("DATA_PATH").unwrap_or(".".to_string());
         let download_dir = format!("{}/downloads", data_path);
         let converted_dir = format!("{}/converted", data_path);
+
+        let ffmpeg_bin = env::var("FFMPEG_BIN").unwrap_or("ffmpeg".to_string());
+        let ffmpeg_args = env::var("FFMPEG_ARGS").unwrap_or(
+            "-y ".to_string()
+                + "-i "
+                + "$INPUT "
+                + "-c:v "
+                + "libx264 "
+                + "-preset "
+                + "veryfast "
+                + "-crf "
+                + "23 "
+                + "-threads "
+                + "4 "
+                + "-c:a "
+                + "copy "
+                + "$OUTPUT",
+        );
 
         let host = env::var("WEBSERVER_HOST").unwrap_or("127.0.0.1".to_string());
         let port = env::var("WEBSERVER_PORT")
@@ -31,6 +51,8 @@ impl Config {
             public_url,
             download_dir,
             converted_dir,
+            ffmpeg_bin,
+            ffmpeg_args,
             host,
             port,
         }
